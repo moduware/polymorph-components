aefaee643336d623726a151ea74c11b20ff3c53e
Clint <clintp1@gmail.com>
2018-08-23 15:14:09 +1000

    Add gatsby template for starters

---
+++ data/SiteConfig.js
@@ -0,0 +1,53 @@
+module.exports = {
+  blogPostDir: "posts", // The name of directory that contains your posts.
+  lessonsDir: "lessons", // The name of the directory that contains lessons or docs.
+  siteTitle: "Polymer Elements", // Site title.
+  siteTitleAlt: "A collection of polymorph elements with their documentation and usage examples. Using Google Polymer creating elements as close to the native webcomponents.", // Alternative site title for SEO.
+  siteLogo: "/logos/c-logo-1024.png", // Logo used for SEO and manifest.
+  siteUrl: "", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: "A collection of polymorph elements that looks native to both IOS and Android platform.", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteFBAppID: "1825", // FB Application ID for using app insights
+  googleAnalyticsID: "UA-82300-1 ", // GA tracking ID.
+  disqusShortname: "https-moduware-github-io-docs-starter", // Disqus shortname.
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  userName: "User", // Username to display in the author segment.
+  userTwitter: "moduwareinc", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Melbourne, AUS", // User location to display in the author segment.
+  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userDescription: "All about us!", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/moduware",
+      iconClassName: "fa fa-github"
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/moduwareinc",
+      iconClassName: "fa fa-twitter"
+    },
+    {
+      label: "Messenger",
+      url: "https://www.messenger.com/t/ModuwareInc",
+      iconClassName: "fa fa-message"
+    },
+    {
+      label: "Email",
+      url: "mailto:info@moduware.com",
+      iconClassName: "fa fa-envelope"
+    },
+    {
+      label: "Instagram",
+      url: "https://instagram.com/moduware",
+      iconClassName: "fa fa-envelope"
+    }
+  ],
+  copyright: "Copyright © 2017. Advanced User", // Copyright string for the footer of the website and RSS feed.
+  themeColor: "#d12f3d", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#d12f3d", // Used for setting manifest background color.
+  // TODO: Move this literally anywhere better.
+  toCChapters: ["", "Chapter 1", "Chapter 2"] // Used to generate the Table Of Contents. Index 0 should be blank.
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,126 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import styled from 'styled-components'
+
+import SEO from '../components/SEO'
+import config from '../../data/SiteConfig'
+import CtaButton from '../components/CtaButton'
+import Navigation from '../components/Layout/Navigation'
+
+class Index extends React.Component {
+  render() {
+    const allSEOMarkdown = this.props.data.allMarkdown.edges
+
+    return (
+      <div className="index-container">
+        <Helmet title={config.siteTitle} />
+        <SEO postEdges={allSEOMarkdown} />
+        <main>
+          <IndexHeadContainer>
+            <Navigation />
+            <Hero>
+              <img src={config.siteLogo} width="150px" alt="" />
+              <h1>{config.siteTitle}</h1>
+              <h4>{config.siteDescription}</h4>
+            </Hero>
+          </IndexHeadContainer>
+          <BodyContainer>
+            <h2>List of Polymorph Elements</h2>
+            <p>
+              A collection of polymorph elements with their documentation and usage examples.{' '}
+            </p>
+            <CtaButton to={'/lesson-one'}>See Our First Element</CtaButton>
+
+            <div className="contributors">
+              <p>
+                Created by Moduware.{' '}
+                <a href="https:twitter.com/moduwareinc">
+                  You should follow us on Twitter.
+                </a>{' '}
+                Also, <a href="https://github.com/moduware">Moduware</a> for
+                our github profile.
+              </p>
+            </div>
+          </BodyContainer>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default Index
+
+const IndexHeadContainer = styled.div`
+  background: ${props => props.theme.brand};
+  padding: ${props => props.theme.sitePadding};
+  text-align: center;
+  color: white;
+`
+
+const Hero = styled.div`
+  padding: 50px 0;
+  & > h1 {
+    font-weight: 600;
+  }
+  & > img {
+    border-radius: 50%;
+  }
+`
+
+const BodyContainer = styled.div`
+  padding: ${props => props.theme.sitePadding};
+  max-width: ${props => props.theme.contentWidthLaptop};
+  margin: 0 auto;
+
+  .contributors {
+    max-width: 400px;
+    margin: 100px auto 0;
+  }
+`
+
+/* eslint no-undef: "off" */
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdown: allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          excerpt
+          timeToRead
+          frontmatter {
+            title
+            tags
+            cover
+            date
+          }
+        }
+      }
+    }
+    posts: allMarkdownRemark(
+      limit: 2000
+      filter: { frontmatter: { type: { eq: "post" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          excerpt
+          timeToRead
+          frontmatter {
+            title
+            tags
+            cover
+            date
+          }
+        }
+      }
+    }
+  }
+`
