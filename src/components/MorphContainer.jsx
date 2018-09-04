8a9f0250e776ab7fcd1aa5c71c02458fdeb27780
Clint <clintp1@gmail.com>
2018-09-04 11:16:47 +1000

    add the rest of morph elements

--- src/components/MorphContainer.jsx
+++ src/components/MorphContainer.jsx
@@ -11,14 +11,18 @@ class MorphContainer extends Component {
           <div className="left">
             <p>
               Android Morph Button
-              <morph-button platform="android" big filled color="red">morph button</morph-button>
+              <p>
+                <morph-button platform="android" big filled color="red">morph button</morph-button>
+              </p>
             </p>
           </div>
 
           <div className="right">
             <p>
               IOS Morph Button
-              <morph-button platform="ios" big filled color="blue">morph button</morph-button>
+              <p>
+                <morph-button platform="ios" big filled color="blue">morph button</morph-button>
+              </p>
             </p>
           </div>
         </MorphColumn>
@@ -29,22 +33,148 @@ class MorphContainer extends Component {
           <div className="left">
             <p>
               Android Morph Tabbar
-              <morph-tabbar platform="android" selected="play" label>
-                <morph-tabbar-item platform="android" name="play" not-selected-image="../images/play_android.svg" selected-image="../images/play_android_selected.svg" has-label>play</morph-tabbar-item>
-                <morph-tabbar-item platform="android" name="favorite" not-selected-image="../images/favorite_android.svg" selected-image="../images/favorite_android_selected.svg" has-label>favorite</morph-tabbar-item>
-                <morph-tabbar-item platform="android" name="mic" not-selected-image="../images/mic_android.svg" selected-image="../images/mic_android_selected.svg" has-label>mic</morph-tabbar-item>
-              </morph-tabbar>
+              <p>
+                <morph-tabbar platform="android" selected="play" label>
+                  <morph-tabbar-item platform="android" name="play" not-selected-image="../images/play_android.svg" selected-image="../images/play_android_selected.svg" has-label>play</morph-tabbar-item>
+                  <morph-tabbar-item platform="android" name="favorite" not-selected-image="../images/favorite_android.svg" selected-image="../images/favorite_android_selected.svg" has-label>favorite</morph-tabbar-item>
+                  <morph-tabbar-item platform="android" name="mic" not-selected-image="../images/mic_android.svg" selected-image="../images/mic_android_selected.svg" has-label>mic</morph-tabbar-item>
+                </morph-tabbar>
+              </p>
             </p>
           </div>
 
           <div className="right">
             <p>
               IOS Morph Tabbar
-              <morph-tabbar platform="ios" selected="play" label>
-                <morph-tabbar-item platform="ios" name="play" not-selected-image="../images/play_ios.svg" selected-image="../images/play_ios_selected.svg" has-label>play</morph-tabbar-item>
-                <morph-tabbar-item platform="ios" name="favorite" not-selected-image="../images/favorite_ios.svg" selected-image="../images/favorite_ios_selected.svg" has-label>favorite</morph-tabbar-item>
-                <morph-tabbar-item platform="ios" name="mic" not-selected-image="../images/mic_ios.svg" selected-image="../images/mic_ios_selected.svg" has-label>mic</morph-tabbar-item>
-              </morph-tabbar>
+              <p>
+                <morph-tabbar platform="ios" selected="play" label>
+                  <morph-tabbar-item platform="ios" name="play" not-selected-image="../images/play_ios.svg" selected-image="../images/play_ios_selected.svg" has-label>play</morph-tabbar-item>
+                  <morph-tabbar-item platform="ios" name="favorite" not-selected-image="../images/favorite_ios.svg" selected-image="../images/favorite_ios_selected.svg" has-label>favorite</morph-tabbar-item>
+                  <morph-tabbar-item platform="ios" name="mic" not-selected-image="../images/mic_ios.svg" selected-image="../images/mic_ios_selected.svg" has-label>mic</morph-tabbar-item>
+                </morph-tabbar>
+              </p>
+            </p>
+          </div>
+        </MorphColumn>
+
+        <h3>morph-list-view and morph-list-view-item</h3>
+
+        <MorphColumn>
+          <div className="left">
+            <p>
+              Android Morph List View
+              <p>
+                <morph-list-view platform="android">
+                  <morph-list-view-title platform="android">First list</morph-list-view-title>
+                  <morph-list-view-item platform="android">Item 1</morph-list-view-item>
+                  <morph-list-view-item platform="android">Item 2</morph-list-view-item>
+                  <morph-list-view-item platform="android">Item 3</morph-list-view-item>
+                </morph-list-view>
+              </p>
+            </p>
+          </div>
+          <div className="right">
+            <p>
+              IOS Morph List View
+              <p>
+                <morph-list-view platform="ios">
+                  <morph-list-view-title platform="ios">First list</morph-list-view-title>
+                  <morph-list-view-item platform="ios">Item 1</morph-list-view-item>
+                  <morph-list-view-item platform="ios">Item 2</morph-list-view-item>
+                  <morph-list-view-item platform="ios">Item 3</morph-list-view-item>
+                </morph-list-view>
+              </p>
+            </p>
+          </div>
+        </MorphColumn>
+
+        <h3>morph-pages</h3>
+        
+        <MorphColumn>
+          <div className="left">
+            <p>
+              Android Morph Pages
+              <p>
+                <img src="/images/morph-pages-ios-demo.gif" alt="Morph Pages element" />
+              </p>
+            </p>
+          </div>
+          <div className="right">
+            <p>
+              IOS Morph Pages
+              <p>
+                <img src="/images/morph-pages-android-demo.gif" alt="Morph Pages element" />
+              </p>
+            </p>
+          </div>
+        </MorphColumn>
+
+        <h3>morph-sidebar</h3>
+
+        <MorphColumn>
+          <div className="left">
+            <p>
+              Android Morph Sidebar
+              <p>
+                <img src="/images/sidebar-demo-android.gif" alt="Morph Sidebar element" />
+              </p>
+            </p>
+          </div>
+          <div className="right">
+            <p>
+              IOS Morph Sidebar
+              <p>
+                <img src="/images/sidebar-demo-ios.gif" alt="Morph Sidebar element" />
+              </p>
+            </p>
+          </div>
+        </MorphColumn>
+
+        <h3>morph-swipeout</h3>
+
+        <MorphColumn>
+          <div className="left">
+            <p>
+              Android Morph swipeout
+              <p align="center">
+                <img src="/images/morph-swipeout-demo.gif" alt="morph-swipeout demo" />
+              </p>
+            </p>
+          </div>
+          <div className="right">
+            <p>
+              IOS Morph
+            </p>
+          </div>
+        </MorphColumn>
+
+        <h3>morph-ripple</h3>
+
+        <MorphColumn>
+          <div className="left">
+            <p>
+              Android Morph Ripple
+              <p align="center">
+                <img src="/images/ripple-tabbar-demo.gif" alt="morph ripple demo image" />
+              </p>
+            </p>
+          </div>
+          <div className="right">
+            <p>
+              IOS Morph
+            </p>
+          </div>
+        </MorphColumn>
+
+        <h3>morph-overlay</h3>
+
+        <MorphColumn>
+          <div className="left">
+            <p>
+              Android Morph Overlay
+              <p>
+                <img src="/images/overlay-android-demo.gif" alt="morph sidebar demo image" />
+              </p>
             </p>
           </div>
         </MorphColumn>
